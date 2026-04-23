76344bb600ddee4e7bddb3504de12429fe8afb40
Stefan Andersson <stefan.andersson@sveki.se>
2026-04-23 11:03:46 +0200

    Add placeholder frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the Receipt Handler!</h1>
+      <p>This is a simple receipt handler application.</p>
+    </>
+  );
+}
+
+export default App;
