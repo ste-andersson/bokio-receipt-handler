58254da85f12cc626803d941df53868fb2cef14b
Stefan Andersson <stefan.andersson@sveki.se>
2026-04-23 19:16:26 +0200

    Add data models and Jackson dependency for Bokio accounting integration

---
+++ backend/src/main/java/se/sveki/receipthandler/model/request/AccountingRequest.java
@@ -0,0 +1,30 @@
+package se.sveki.receipthandler.model.request;
+
+import java.util.List;
+
+public class AccountingRequest {
+
+    private String title;
+    private String date;
+    private List<JournalItem> items;
+
+    public static class JournalItem {
+        private int account;
+        private double debit;
+        private double credit;
+
+        public int getAccount() { return account; }
+        public void setAccount(int account) { this.account = account; }
+        public double getDebit() { return debit; }
+        public void setDebit(double debit) { this.debit = debit; }
+        public double getCredit() { return credit; }
+        public void setCredit(double credit) { this.credit = credit; }
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public List<JournalItem> getItems() { return items; }
+    public void setItems(List<JournalItem> items) { this.items = items; }
+}
